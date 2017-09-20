ea22a14a0c53b7699de152dc9481edefcc25372d
Y.J <yuxuan98@hotmail.com>
2017-09-19 22:59:47 -0400

    __main__ moved to run.py

--- run.py
+++ run.py
@@ -2,5 +2,7 @@ from BubuBot import BubuBot
 import os
 
 if __name__ == '__main__':
+	import __main__
+	__main__.DEBUG = False
 	BOT_TOKEN = os.environ["DISCORD_TOKEN"]  # You must first use heroku config:set DISCORD_TOKEN=YOURBOTTOKEN in your shell
 	BubuBot(command_prefix='!b ').run(BOT_TOKEN)
