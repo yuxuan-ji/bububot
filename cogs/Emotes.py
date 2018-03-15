e1f5e406bf330ad08232999fadd50bb4490fbc82
Y.J <yuxuan98@hotmail.com>
2018-03-14 21:51:27 -0400

    added redis caching

--- cogs/Emotes.py
+++ cogs/Emotes.py
@@ -1,6 +1,8 @@
 from discord.ext import commands
 from .utils.Posts import postAcceptedInputs, postEmbedImg
 from .utils.Checks import is_admin
+import redis
+import os
 
 
 class Emotes:
@@ -9,6 +11,7 @@ class Emotes:
     def __init__(self, client):
         '''Client = the bot'''
         self.client = client
+        self.redis_conn = None
         self.logger = self.client.logger
         # SQL table config:
         # NOTE: execute DROP TABLE locally to change columns config, will lose all data though.
@@ -26,6 +29,13 @@ class Emotes:
             self.conn, self.c = get_conn()
             self.client.logger.debug("Using auto connection")
         self.c.execute("CREATE TABLE IF NOT EXISTS waifus (name TEXT, emote TEXT, url TEXT)")
+
+    @commands.command(pass_context=True)
+    @commands.check(is_admin)
+    async def activate_redis_cache(self, ctx, name, emote, url):
+        host, port = os.environ["REDIS_URL"].split(":")
+        pw = os.environ["REDIS_PW"]
+        self.redis_conn = redis.StrictRedis(host=host, port=int(port), password=pw)
 
     @commands.command(pass_context=True)
     @commands.check(is_admin)
@@ -69,7 +79,7 @@ class Emotes:
     async def emote(self, ctx, *args):
         '''<name> <emote> : Summons the emote'''
         if len(args) < 2:
-            # Show all available emot and emotes:
+            # Show all available emotes:
             with self.conn:
                 try:
                     self.c.execute('SELECT * FROM waifus')
@@ -83,17 +93,24 @@ class Emotes:
         else:
             name = args[0]
             emote = args[1]
-            with self.conn:
-                try:
-                    # Grab the url and post it using an embed:
-                    self.c.execute('SELECT * FROM waifus WHERE name = %(name)s AND emote = %(emote)s',
-                                   {'name': name, 'emote': emote})
-                    entry = self.c.fetchone()  # returns a tuple (field1, field2, ...) or None
-                    assert(isinstance(entry, tuple))
-                    entry_url = entry[2]
+            if self.redis_conn is not None:
+                entry_url = self.redis_conn.hget(name, emote).decode()  # b string
+                if entry_url is not None:
                     await postEmbedImg(client=self.client, url=entry_url)
-                except AssertionError:
-                    await self.client.say("Not an entry in the database")
+            else:
+                with self.conn:
+                    try:
+                        # Grab the url and post it using an embed:
+                        self.c.execute('SELECT * FROM waifus WHERE name = %(name)s AND emote = %(emote)s',
+                                       {'name': name, 'emote': emote})
+                        entry = self.c.fetchone()  # returns a tuple (field1, field2, ...) or None
+                        assert(isinstance(entry, tuple))
+                        entry_url = entry[2]
+                        await postEmbedImg(client=self.client, url=entry_url)
+                        if self.redis_conn is not None:
+                            self.redis_conn.hset(name, emote, entry_url)
+                    except AssertionError:
+                        await self.client.say("Not an entry in the database")
 
 
 def setup(client):
